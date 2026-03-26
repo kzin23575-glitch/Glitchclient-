30e4f5a856f6387c6e252e4cda898742ba82b6bf
kzin23575-glitch <kzin23575@gmail.com>
2026-03-26 11:37:56 +0630

    Create clickGUI.js

---
+++ gui/clickGUI.js
@@ -0,0 +1,13 @@
+const gui = document.createElement("div");
+
+gui.style.position = "fixed";
+gui.style.top = "20px";
+gui.style.right = "20px";
+gui.style.background = "black";
+gui.style.color = "lime";
+gui.style.padding = "10px";
+gui.style.zIndex = "9999";
+
+gui.innerHTML = "<h3>GlitchClient</h3>";
+
+document.body.appendChild(gui);
